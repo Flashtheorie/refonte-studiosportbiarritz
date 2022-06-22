df30fdbb2a5d9a5b692a706be8309a52b2b27255
Antoine Pascual <antoinepascual@Antoines-MacBook-Pro.local>
2022-06-22 14:01:57 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+    <title>Accueil</title>
+</head>
+<body>
+
+
+
+
+<div class="navbar">
+    <div class="logo">
+        <img src="assets/img/logo.png" alt="logo" id="imglogo">
+    </div>
+        <ul class="menuList">
+            <li>Présentations</li>
+            <li>Les activités</li>
+            <li>Nos offres</li>
+            <li>Planning</li>
+            <li>Le blog</li>
+            <li>Contact</li>
+        </ul>
+    
+</div>
+
+<div class="hero-image">
+  <div class="hero-text">
+    <h1>nos offres et tarifs</h1>
+    <p>DES FORMULES POUR TOUS LES BUDGETS
+ET POUR RÉPONDRE À TOUTES VOS ATTENTES</p>
+    <button>En savoir plus</button>
+  </div>
+</div>
+
+
+
+<div class="content">
+    <div class="contenttitre">
+        le studio sport et coaching
+        <br>
+        SALLE DE SPORT ET DE FITNESS À BIARRITZ
+    </div>
+    <hr class="hr">
+    <div class="contenttext">
+    <p>Aujourd'hui, beaucoup de salles de sport vendent des abonnements où vous avez accès à tous les services du club (cours collectifs, plateau musculation…) mais combien vous connaissent au point de connaitre vos objectifs et de savoir si vous êtes en bonne voie pour les atteindre ?</p>
+    <p>Beaucoup de gens estiment être livrés à eux même dans ce genre de salle.
+        <span class="gras"> Au Studio Sport & Coaching de Biarritz, nous avons choisi la direction diamétralement opposée !</span> Notre seule priorité ? La qualité du Service.</p>
+    <p>Avec notre nouveau concept de <span class="gras"> salle de sport sur-mesure </span>,vous avez l'assurance de trouver une solution et des formules adaptées à votre budget et vos disponibilités. Nous oeuvrons pour créer une ambiance conviviale tout en vous aidant à vous dépasser dans l'effort et ce, pour maximiser vos chances d'obtenir des résultats. Les sportifs de Haut-Niveau ont des entraîneurs et des préparateurs physiques pour atteindre leurs objectifs. Alors, pourquoi pas vous ?</p>
+    
+    <p>Vous voulez plus de Motivation, plus de Résultats, plus vite… <span class="gras"> Alors bienvenue au Studio Sport & Coaching !</span></p>
+    </div> <!-- content text -->
+
+
+
+
+
+
+    <!-- Deuxième partie -->
+
+    <div class="contenttitre">
+    LES ACTIVITÉS PROPOSÉES AU STUDIO SPORT BIARRITZ
+    </div>
+    <hr class="hr">
+
+
+
+
+    <div id="triplephotos">
+        <div id="photo1">
+            <div class="icon">
+                <img src="assets/img/icon_poids.png" alt="">
+            </div>
+            <div class="icontitre">
+            PERSONAL TRAINING
+            </div>
+            
+            <div class="icontext">
+            100% adapté à vos disponibilités, votre condition et vos objectifs. La formule idéale si vous voulez un coach de fitness personnel.
+            </div>
+            <button>En savoir plus</button>
+        </div>
+
+
+        <div id="photo2">
+            <div class="icon">
+                <img src="assets/img/icon_velo.png" alt="">
+            </div>
+            <div class="icontitre">
+            FIT CYCLE
+            </div>
+            
+            <div class="icontext">
+            Le Studio Sport Biarritz vous propose un espace dédié au Cycling 2.0 : musique rythmée et séances adaptées à votre niveau et vos attentes.
+            </div>
+            <button>En savoir plus</button>
+        </div>
+
+        <div id="photo3">
+            <div class="icon">
+                <img src="assets/img/icon_corde.png" alt="">
+            </div>
+            <div class="icontitre">
+            FUNCTIONAL TRAINING
+            </div>
+            
+            <div class="icontext">
+            Des séances intenses en petit groupe (8 personnes max.), différents ateliers (boxe, TRX, HIIT...) pour des résultats garantis !
+            </div>
+            <button>En savoir plus</button>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+<div class="contenttitre">
+LES COACHS DU STUDIO BIARRITZ
+    </div>
+    <hr class="hr">
+
+
+    
+<div class="formateurs">
+    <div class="formateursphoto1"><img src="assets/img/" alt=""></div>
+    <div class="formateursphoto2"></div>
+</div>
+
+    </div> <!-- #content -->
+
+
+
+</body>
+</html>
